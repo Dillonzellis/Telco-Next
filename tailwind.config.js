a7eb4aadea4170f9485d280665884900fdf54b25
Dillon Ellis <dillonzellis@gmail.com>
2023-03-10 13:36:50 -0500

    add largest screen max width for container class

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1350px",
+    },
     extend: {
       colors: {
         brandingBlue: {
